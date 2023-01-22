b37eea718d8b592df4264ea66d996079b800274a
Martini <kailing2@live.com>
2023-01-22 10:02:41 +0000

    Changes related to termux -> nhterm

--- app/src/main/jni/libxkbcommon/xkbcommon/src/xkbcomp/config.h
+++ app/src/main/jni/libxkbcommon/xkbcommon/src/xkbcomp/config.h
@@ -1,6 +1,6 @@
 #define _GNU_SOURCE_ 1
 
-#define DFLT_XKB_CONFIG_EXTRA_PATH "/data/data/com.termux/files/home/.termux.wayland/xkb/config"
+#define DFLT_XKB_CONFIG_EXTRA_PATH "/data/data/com.offsec.nhterm/files/usr/home/.nhterm/xkb/config"
 
 #define DEFAULT_XKB_VARIANT "NULL"
 

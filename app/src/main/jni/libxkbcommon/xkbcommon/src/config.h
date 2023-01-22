9f8883f869a1c5b364ad6ca93d9d1063dbfb0125
Martini <kailing2@live.com>
2023-01-22 10:32:21 +0000

    Prefer nhbiew tmp for wayland nodez

--- app/src/main/jni/libxkbcommon/xkbcommon/src/config.h
+++ app/src/main/jni/libxkbcommon/xkbcommon/src/config.h
@@ -1,6 +1,6 @@
 #define _GNU_SOURCE_ 1
 
-#define DFLT_XKB_CONFIG_EXTRA_PATH "/data/data/com.offsec.nhterm/files/usr/home/.nhterm/xkb/config"
+#define DFLT_XKB_CONFIG_EXTRA_PATH "/data/data/com.offsec.nhview/files/usr/home/.nhview/xkb/config"
 
 #define DEFAULT_XKB_VARIANT "NULL"
 
